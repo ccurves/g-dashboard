3a318b3ae41134dafdc70d7ec36edac49ba5d186
ccurves <70116851+ccurves@users.noreply.github.com>
2023-02-07 13:52:59 +0100

    Fixed monogdb error

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const connection = mongoose.connect(process.env.MONGO_URL, {
+  const connection = await mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
